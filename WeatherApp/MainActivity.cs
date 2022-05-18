4ad268170feeb85a47193c033e4e232d730a6acb
gregorkookmaa <gregor.kookmaa@tptlive.ee>
2022-05-18 14:07:25 +0300

    Git init

---
+++ WeatherApp/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using WeatherApp.Services;
+
+namespace WeatherApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            var cityEditText = FindViewById<EditText>(Resource.Id.cityEditText);
+            var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+            var temperatureTextView = FindViewById<TextView>(Resource.Id.TemperatureTextView);
+            var windTextView = FindViewById<TextView>(Resource.Id.WindTextView);
+            var weatherImageView = FindViewById<ImageView>(Resource.Id.weatherImageView);
+
+            var weatherService = new WeatherService();
+
+            searchButton.Click += async delegate
+            {
+                var data = await weatherService.GetCityWeather(cityEditText.Text);
+                temperatureTextView.Text = data.main.temp.ToString() + "°";
+                windTextView.Text = data.wind.speed.ToString() + "m/s";
+
+                var imageBytes = await weatherService.GetImageFromUrl($"https://openweathermap.org/img/wn/{data.weather[0].icon}@2x.png");
+                var bitmap = await BitmapFactory.DecodeByteArrayAsync(imageBytes, 0, imageBytes.Length);
+                weatherImageView.SetImageBitmap(bitmap);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ WeatherApp/Services/WeatherService.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class WeatherService
+    {
+        const string ApiKey = "0251e7e04eae543d5788b8a5f3a08a44"; 
+
+        public async Task<WeatherInfo> GetCityWeather(string city)
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetStringAsync
+                ($"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={ApiKey}");
+                var data = JsonConvert.DeserializeObject<WeatherInfo>(response);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public async Task<Byte[]> GetImageFromUrl(string url)
+        {
+            using (var client = new HttpClient()) 
+            {
+                var msg = await client.GetAsync(url);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var byteArray = await msg.Content.ReadAsByteArrayAsync();
+                    return byteArray;
+                }
+                return null;
+            }
+        }
+    }
+}
